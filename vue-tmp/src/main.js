cadb4dc570e798b951a0473b118016c62d0403d2
fesoa <fernandofesoa@gmail.com>
2019-02-07 12:12:42 +0900

    +

---
+++ vue-tmp/src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+//
+const Login = { template: '<div>Login Page</div>' }
+
+const routes = {
+  '/': App,
+  '/login': Login
+}
+
+new Vue({
+  el: '#app',
+  computed: {
+    VueComponent() {
+      return routes[window.location.pathname] || { template: '<div>Pange not found</div>' }
+    }
+  },
+  
+  render(h) {
+    return h(this.VueComponent)
+  }
+})
