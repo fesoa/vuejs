bcc1b9d18c9fd980c752377813a70b39667161b4
fesoa <fernandofesoa@gmail.com>
2019-02-07 17:21:11 +0900

    +

--- vue-tmp/src/main.js
+++ vue-tmp/src/main.js
@@ -1,23 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+// Vue Router 추가
+import VueRouter from 'vue-router'
 
-//
+Vue.use(VueRouter)
+
+// 라우트 컴포넌트 정의
 const Login = { template: '<div>Login Page</div>' }
+const NotFound = { template: '<div>Not Found</div>' }
+const Print = { template: '<div>Print Page</div>'}
 
-const routes = {
-  '/': App,
-  '/login': Login
-}
+// 라우터 인스턴스
+const router = new VueRouter({
+  mode: 'history', //크롬의 history 모드 사용해서 url에 #이 사용되는것 막음
+  routes: [
+    { path: '/', component: App},
+    { path: '/login', component: Login},
+    { path: '/print', component: Print},
+    { path: '*', component: NotFound} //순서대로 매칭 시키다 여기 없으면 이것으로
+  ] // routes 정의
+})
 
+// 루트 인스턴스
 new Vue({
   el: '#app',
-  computed: {
-    VueComponent() {
-      return routes[window.location.pathname] || { template: '<div>Pange not found</div>' }
-    }
-  },
-  
-  render(h) {
-    return h(this.VueComponent)
-  }
+  router,
+  render: h => h({template: '<router-view></router-view>'})
 })
